8031c9bb3f7b5a730be146e552ccffcf6a119a9e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-25 19:32:59 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,13 @@ end
 
 # Define your play method below
 def play(board)
-  while turn(board) < 9
-    turn(board)
-  end
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
+  turn(board)
 end
